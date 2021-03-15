8c39e668c18ccd91d5bf130580269a67250d0c7b
xebeto <anhtuana21417@gmail.com>
2021-03-15 16:58:33 +0700

    First commit

---
+++ src/main/java/com/example/web_sell_fruit/WebSellFruitApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_sell_fruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSellFruitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSellFruitApplication.class, args);
+    }
+
+}
