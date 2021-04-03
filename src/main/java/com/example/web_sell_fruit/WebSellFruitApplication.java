b2601c894fd0a4d656ca43abcb55290cdecbcc7e
xebeto <anhtuana21417@gmail.com>
2021-04-03 08:19:44 +0700

    commit Frontend Home Page

--- src/main/java/com/example/web_sell_fruit/WebSellFruitApplication.java
+++ src/main/java/com/example/web_sell_fruit/WebSellFruitApplication.java
@@ -2,8 +2,9 @@ package com.example.web_sell_fruit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class WebSellFruitApplication {
 
     public static void main(String[] args) {
