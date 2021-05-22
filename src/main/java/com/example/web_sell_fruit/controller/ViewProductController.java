d5b8d59a281cb4cb35d8d0952346085f523d3d53
xebeto <anhtuana21417@gmail.com>
2021-05-22 10:05:51 +0700

    commit linh tinh

--- src/main/java/com/example/web_sell_fruit/controller/ViewProductController.java
+++ src/main/java/com/example/web_sell_fruit/controller/ViewProductController.java
@@ -1,22 +1,33 @@
 package com.example.web_sell_fruit.controller;
 
+import com.example.web_sell_fruit.models.CategoryDTO;
 import com.example.web_sell_fruit.models.ProductDTO;
+import com.example.web_sell_fruit.service.CategoryService;
 import com.example.web_sell_fruit.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 
 @Controller
 public class ViewProductController {
     @Autowired
     private ProductService productService;
+    @Autowired
+    private CategoryService categoryService;
 
     @GetMapping(value = "/detailproduct/{id}")
     public String viewDetailProduct(HttpServletRequest request, @PathVariable("id") int id) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        request.setAttribute("authentication",authentication.getName());
+        List<CategoryDTO> categoryDTOs = categoryService.getList();
+        request.setAttribute("listCategory", categoryDTOs);
         ProductDTO productDTO = productService.getById(id);
 
         request.setAttribute("product", productDTO);
